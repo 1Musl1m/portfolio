257b61835450abe570bfd5e8b454a01e6fcbc19b
Саша <115547303+1Musl1m@users.noreply.github.com>
2024-06-05 17:23:41 +0300

    second commit

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -1,9 +1,31 @@
-import { Button } from "antd";
-import Link from "next/link";
+'use client'
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+    const [navbarClass, setNavbarClass] = useState<string>('');
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const specializationSection = document.getElementById('specialization');
+            if (specializationSection) {
+                const aboutTop = specializationSection.getBoundingClientRect().top;
+                if (aboutTop <= 50) { 
+                    setNavbarClass('navbar-white-text');
+                } else {
+                    setNavbarClass('');
+                }
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className="w-full flex items-center justify-between fixed top-0 bg-opacity-80 backdrop-blur-md z-10 p-9">
+        <nav className={`w-full flex items-center justify-between fixed top-0 bg-opacity-80 backdrop-blur-md z-10 p-9 ${navbarClass}`}>
             <Link href="/">
                 <h1 className="text-2xl font-bold">
                     <span className="text-red-800 font-semibold">/</span>Ashanti
@@ -11,39 +33,24 @@ const Navbar = () => {
             </Link>
             <ul className="flex items-center justify-end text-xl gap-6 font-medium">
                 <li>
-                    <Link
-                        href="#"
-                        className=" transition-all duration-100 hover:border-b-2 border-gray-800"
-                    >
+                    <Link href="#home" className="transition-all duration-100 hover:border-b-2 border-gray-800">
                         Home
                     </Link>
                 </li>
                 <li>
-                    <Link
-                        href="#"
-                        className="transition-all duration-100 hover:border-b-2 border-gray-800"
-                    >
-                        About
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        href="#"
-                        className=" transition-all duration-100 hover:border-b-2 border-gray-800"
-                    >
+                    <Link href="#specialization" className="transition-all duration-100 hover:border-b-2 border-gray-800">
                         Specialization
                     </Link>
                 </li>
                 <li>
-                    <Link
-                        href="#"
-                        className=" transition-all duration-100 hover:border-b-2 border-gray-800"
-                    >
+                    <Link href="#contacts" className="transition-all duration-100 hover:border-b-2 border-gray-800">
                         Contacts
                     </Link>
                 </li>
             </ul>
-            <button className="text-lg border-2 p-2 px-3 rounded border-black font-medium"><Link href='#'>Hire me</Link></button>
+            <button className="text-lg border-2 p-2 px-3 rounded border-black font-medium">
+                <Link href='#'>Hire me</Link>
+            </button>
         </nav>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,103 @@
 import Image from "next/image";
 
-const Home = () => {
-  return (
-    <div className="container h-screen p-3 flex items-center bg-gradient-to-br from-white to-red-700 pt-28">
-      <div className="p-16 w-1/2">
-        <h1 className="text-3xl font-semibold">HI, I'M Muslim</h1>
-        <h2 className="text-7xl font-bold">I'M A <br/>WEB/APP <br/> DEVELOPER</h2>
-        <p className="text-lg text-gray-900">I create beautiful and interactive websites. My technology <br/> stack includes HTML, CSS, and JavaScript. I am proficient <br/> in working with frameworks such as React and Vue.js and <br/> creating responsive layouts.</p>
-      </div>
-      <div>
-        <img src="/coder.svg" alt="image" width={600} height={400}></img>
-      </div>
-    </div>
-  );
+const Home: React.FC = () => {
+    return (
+        <div>
+            <div className="container h-screen p-3 flex items-center bg-gradient-to-br from-white to-red-700 pt-28">
+                <div className="p-16 w-1/2">
+                    <h1 className="text-3xl font-semibold">HI, I'M Sasha</h1>
+                    <h2 className="text-7xl font-bold">
+                        I'M A <br />
+                        WEB/APP <br /> DEVELOPER
+                    </h2>
+                    <p className="text-lg text-gray-900">
+                        I create beautiful and interactive websites. My
+                        technology <br /> stack includes HTML, CSS, and
+                        JavaScript. I am proficient <br /> in working with
+                        frameworks such as React and Vue.js and <br /> creating
+                        responsive layouts.
+                    </p>
+                </div>
+                <div>
+                    <Image
+                        src="/coder.svg"
+                        alt="image"
+                        width={600}
+                        height={400}
+                    />
+                </div>
+            </div>
+            <div
+                id="specialization"
+                className="h-screen w-full text-white bg-black flex items-center justify-center gap-10"
+            >
+                <div className="block max-w-sm p-6 border rounded-lg shadow bg-black border-white">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
+                        Noteworthy technology acquisitions 2021
+                    </h5>
+                    <p className="font-normal">
+                        Here are the biggest enterprise technology acquisitions
+                        of 2021 so far, in reverse chronological order.
+                    </p>
+                </div>
+
+                <div className="block max-w-sm p-6 border rounded-lg shadow bg-black border-white">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
+                        Noteworthy technology acquisitions 2021
+                    </h5>
+                    <p className="font-normal">
+                        Here are the biggest enterprise technology acquisitions
+                        of 2021 so far, in reverse chronological order.
+                    </p>
+                </div>
+
+                <div className="block max-w-sm p-6 border rounded-lg shadow bg-black border-white">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
+                        Noteworthy technology acquisitions 2021
+                    </h5>
+                    <p className="font-normal">
+                        Here are the biggest enterprise technology acquisitions
+                        of 2021 so far, in reverse chronological order.
+                    </p>
+                </div>
+            </div>
+
+            <div
+                id="contacts"
+                className="h-screen w-full text-white bg-gradient-to-br from-black to-white"
+            >
+                <div className="flex items-center justify-center flex-col h-full">
+                    <h1 className="text-9xl font-bold">Contact</h1>
+                    <div className="flex gap-4">
+                        <a
+                            href="https://github.com/1Musl1m"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="social-link"
+                        >
+                          <Image src="/github.svg" alt="image" width={50} height={50}></Image>
+                        </a>
+                        <a
+                            href="https://twitter.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="social-link"
+                        >
+                            <Image src="/telegram.svg" alt="image" width={50} height={50}></Image>
+                        </a>
+                        <a
+                            href="https://instagram.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="social-link "
+                        >
+                            <Image src="/insta.svg" alt="image" width={50} height={50}></Image>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
